6c5db28eda28d6f23084c292c82e90b5092ea252
Mynor-Alonso-1992 <136778116+Mynor-Alonso-1992@users.noreply.github.com>
2024-03-01 19:01:42 -0600

    Add files via upload

---
+++ TAREA PROGRA DESAFIOS/Tarea_Desafios/DESAFIO 1/desafio1_Metodo_static.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        //Solicitar nombre del usuario
+        Console.WriteLine("Introduzca su nombre:");
+        string nombreUsuario = Console.ReadLine();
+
+        //Solicitar ingresos de los últimos 3 meses
+        int[] ingresos = new int[3];
+        for (int i = 0; i < ingresos.Length; i++)
+        {
+            Console.WriteLine("Introduzca los ingresos del mes " + (i + 1) + ":");
+            ingresos[i] = int.Parse(Console.ReadLine());
+        }
+
+        //Calcular la suma y el promedio
+        int suma = 0;
+        foreach (int ingreso in ingresos)
+        {
+            suma += ingreso;
+        }
+
+        double promedio = (double)suma / ingresos.Length;
+
+        //Mostrar el resultado
+        Console.WriteLine("Hola " + nombreUsuario + ", en total ganaste " + suma + " y promediaste " + promedio);
+    }
+}
